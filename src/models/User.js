29050698bbccf88aea958208616492e42ae38765
KeilynRosasDev <keilynrosasprofissional@gmail.com>
2025-12-04 14:28:55 -0300

    fix:Correção para os usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,16 @@ const userSchema = new mongoose.Schema({
     registration: { type: String },
     stage: { type: Number, default: 0 },
     subStage: { type: String, default: '' },
-    lastInteraction: { type: Date, default: Date.now }
+    lastInteraction: { type: Date, default: Date.now },
+    context: { 
+        type: mongoose.Schema.Types.Mixed, 
+        default: { 
+            attempts: 0,
+            menuAttempts: 0,
+            financeAttempts: 0,
+            academicAttempts: 0
+        } 
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
